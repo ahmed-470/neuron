f20a3485859685673aa177c7e7037a8a3e3ec328
khajaraouf <khajaraouf@gmail.com>
2023-04-06 14:46:36 +0100

    code organised

---
+++ or.py
@@ -0,0 +1,36 @@
+from utils.model import Perceptron
+from utils.all_utils import prepare_data, save_plot, save_model 
+import pandas as pd
+
+
+def main(data, eta, epochs, filename, plotFilename):
+    
+
+    df = pd.DataFrame(data)
+
+    print(df)
+
+    X,y = prepare_data(df)
+
+    model = Perceptron(eta=eta, epochs=epochs)
+    model.fit(X, y)
+
+    _ = model.total_loss()
+
+    save_model(model, filename=filename)
+    save_plot(df, plotFilename, model)
+
+
+
+if __name__ == '__main__': # << entry point
+
+    OR = { 
+    "x1": [0,0,1,1],
+    "x2": [0,1,0,1],
+    "y": [0,1,1,1],
+    }
+
+    ETA = 0.3 # 0 and 1
+    EPOCHS = 10
+
+    main(data=OR, eta=ETA, epochs=EPOCHS, filename="or.model", plotFilename="or.png")
